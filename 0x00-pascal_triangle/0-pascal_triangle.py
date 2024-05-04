9d74160d532ae0fde63af18a3a53af20063a9d4b
Michael Solomon <Michaelelsa12@gmail.com>
2024-05-04 16:15:44 +0300

    new pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,33 @@
+#!/usr/bin/python3
+"""
+return an empty list if n <= 0
+an empty list to append the lists
+loop from range 1 to n
+create two lists. one for the loop and the other with final results
+first list will be [1]
+the next will be [0, 1, 0]
+create a new loop from 1 to len next list - 1.
+we use the second list to add the two items together
+"""
+
+
 def pascal_triangle(n):
+    """The function to use for pascal's triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)
-        triangle.append(new_row)
+    pascal_list = [[1]]
+    temp = [0, 1, 0]
 
-    return triangle
+    for i in range(1, n):
+        pascal = [1]
+
+        if i > 0:
+            for j in range(1, len(temp) - 1):
+                pascal.append(temp[j] + temp[j + 1])
+
+        temp = [0] + pascal + [0]
+
+        pascal_list.append(pascal)
+
+    return pascal_list
